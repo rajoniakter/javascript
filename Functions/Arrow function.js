00cc6b46d86b0783bc2878490022cceb3b1efc07
rajoniakter <rajoniakter1709@gmail.com>
2022-06-26 00:13:00 +0600

    arrow function

---
+++ Functions/Arrow function.js
@@ -0,0 +1,13 @@
+const name = (a,b) =>{
+    console.log("my name is " + a + " " + b)
+}
+
+name("rajoni" , "akter")
+
+//  write a function that return addition of A and B
+
+const add = (A,B) => {
+    console.log(A + B)
+}
+
+add(24 , 12)
