e0174f9190f76dba845bc2febc70eb7d39054c63
rajoniakter <rajoniakter1709@gmail.com>
2022-06-26 00:11:29 +0600

    variables

---
+++ variable.js
@@ -0,0 +1,12 @@
+const name = "rajoni";
+
+const lastName = "akter"
+
+console.log("hello "  + name + " " +lastName);
+
+console.log(`hello ${name} ${lastName}`);
+
+const age = 21
+let age1 = 20
+age1 = 24
+console.log("age=" + age1)
