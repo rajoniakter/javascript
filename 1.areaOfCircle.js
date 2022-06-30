445c4daa11ca8ac86f41fbd63c7845c1325e9d95
rajoniakter <rajoniakter1709@gmail.com>
2022-06-30 23:34:55 +0600

    area of circle

---
+++ 1.areaOfCircle.js
@@ -0,0 +1,13 @@
+const areaOfCircle = (r) =>{
+   
+    const area = 3.1416 * r *r ;
+   
+    console.log ("Area of circle is " + area  );
+
+}
+
+
+areaOfCircle(7)
+
+//formula of area is Pi*r^2
+// Area of Circle is  314.16
